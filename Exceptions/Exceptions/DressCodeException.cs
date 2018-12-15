20a1201d05e39e901feca9f639a036827f5fbd0a
Ramona Mihai <ramona.mihai87@yahoo.com>
2018-12-15 12:16:47 +0200

    Added costum exception

---
+++ Exceptions/Exceptions/DressCodeException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exceptions
+{
+    class DressCodeException : ApplicationException
+    {
+        string message = "You are not wearing an Elf costume and you are underage!";
+
+        public DressCodeException()
+        {
+            Console.WriteLine(message);
+        }
+
+    }
+}
